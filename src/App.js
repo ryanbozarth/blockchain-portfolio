82d68c12d58c1d49c9f765cfcc678bf487990a31
Ryan Bozarth <ryan_bozarth@ryan-bozarth.local>
2017-08-28 12:18:53 +0800

    added router and started first component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router,
+         Route,
+         Switch
+       } from 'react-router-dom'
+import 'normalize.css';
 import './App.css';
+
+import Header from './Header'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div>
+            <Switch>
+              <Route exact path='/' component={Header} />
+            </Switch>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
