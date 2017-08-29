b04f2be689cfc0f5f03d83abc2dafe48e234c4b3
Ryan Bozarth <ryan.bozarth@gmail.com>
2017-08-29 14:20:33 +0800

    working on layout of asset prices

--- src/pages/home/Content.js
+++ src/pages/home/Content.js
@@ -7,7 +7,24 @@ class Content extends Component {
   render() {
     return (
       <div className="content">
-        <h1>Content</h1>
+        <div className="card">
+          <h3>Portfolio Makeup</h3>
+        </div>
+        <div className="card">
+          <h3>Asset Prices</h3>
+          <div className="card-row">
+            <p>Bitcoin</p>
+            <p className="primary-heading">$4,321.32</p>
+          </div>
+          <div className="card-row">
+            <p>Ethereum</p>
+            <p className="primary-heading">$356.95</p>
+          </div>
+          <div className="card-row">
+            <p>Litecoin</p>
+            <p className="primary-heading">$61.84</p>
+          </div>
+        </div>
       </div>
     );
   }
